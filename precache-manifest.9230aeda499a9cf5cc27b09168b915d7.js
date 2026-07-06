15d57f4b34ff5b6cc8b7e031a7ea02f10458ffe2
Karthik Vijapurapu <karthikvijapurapu@Lucy.local>
2026-07-06 23:03:22 +0530

    Force deploy without colour temp

---
+++ precache-manifest.9230aeda499a9cf5cc27b09168b915d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1a7aa1527dd2ea6b612c3109e237d4",
+    "url": "/voice-zero-mascot-demo/index.html"
+  },
+  {
+    "revision": "1a6f582c0621d098006a",
+    "url": "/voice-zero-mascot-demo/static/css/main.d6136f46.chunk.css"
+  },
+  {
+    "revision": "ccc000df13264850a833",
+    "url": "/voice-zero-mascot-demo/static/js/2.1c5a59c3.chunk.js"
+  },
+  {
+    "revision": "23b26446db404c4cd1d2ae5ba4f80a38",
+    "url": "/voice-zero-mascot-demo/static/js/2.1c5a59c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a6f582c0621d098006a",
+    "url": "/voice-zero-mascot-demo/static/js/main.c9169d46.chunk.js"
+  },
+  {
+    "revision": "5e9c9beaa1273e21acde",
+    "url": "/voice-zero-mascot-demo/static/js/runtime-main.fcee0085.js"
+  }
+]);
