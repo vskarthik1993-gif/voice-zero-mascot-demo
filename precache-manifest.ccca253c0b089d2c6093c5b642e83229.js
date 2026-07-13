e4c9a1d68b67496102ca4f8cafd01f9f434d28ee
Karthik Vijapurapu <karthikvijapurapu@Lucy.local>
2026-07-13 13:30:59 +0530

    Deploy: smoother edges, FXAA, reflective finishes with light pickup

---
+++ precache-manifest.ccca253c0b089d2c6093c5b642e83229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6763c1d4de87e35e0dc62729555af06f",
+    "url": "/voice-zero-mascot-demo/index.html"
+  },
+  {
+    "revision": "9b0cf952ac915eb5e880",
+    "url": "/voice-zero-mascot-demo/static/css/main.7e871fbd.chunk.css"
+  },
+  {
+    "revision": "2c7bcadc8ff3d2e3ac23",
+    "url": "/voice-zero-mascot-demo/static/js/2.fe75abe0.chunk.js"
+  },
+  {
+    "revision": "23b26446db404c4cd1d2ae5ba4f80a38",
+    "url": "/voice-zero-mascot-demo/static/js/2.fe75abe0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b0cf952ac915eb5e880",
+    "url": "/voice-zero-mascot-demo/static/js/main.1d5d464b.chunk.js"
+  },
+  {
+    "revision": "5e9c9beaa1273e21acde",
+    "url": "/voice-zero-mascot-demo/static/js/runtime-main.fcee0085.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voice-zero-mascot-demo/precache-manifest.3810d5339465c1810afc9b154bb49968.js"
+  "/voice-zero-mascot-demo/precache-manifest.ccca253c0b089d2c6093c5b642e83229.js"
 );
 
 self.addEventListener('message', (event) => {
